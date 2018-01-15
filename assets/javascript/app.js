1c0781cb9cdd28ed86bb1d00a7f7da0f04516032
Shawn Feiz <shawnfeiz@gmail.com>
2018-01-15 13:27:48 -0600

    Initilize firebase. Connected.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB9oheZRQxgT2478J7wyALgTAx3Zhfk1Jk",
+    authDomain: "trainschedular.firebaseapp.com",
+    databaseURL: "https://trainschedular.firebaseio.com",
+    projectId: "trainschedular",
+    storageBucket: "trainschedular.appspot.com",
+    messagingSenderId: "376858205018"
+  };
 
-
-
+firebase.initializeApp(config);
 
 $(".submitBtn").on("click", function(event){
     event.preventDefault();
