bf0669af9e704ca5d536996ed238eadf5705c5e7
Shawn Feiz <shawnfeiz@gmail.com>
2018-01-15 20:46:08 -0600

    add a clear button to the form.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-
+    
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyB9oheZRQxgT2478J7wyALgTAx3Zhfk1Jk",
@@ -11,23 +11,18 @@ var config = {
     messagingSenderId: "376858205018"
   };
 
-
 firebase.initializeApp(config);
-
 
 database = firebase.database();
 
-
 $(".submitBtn").on("click", function(event){
     event.preventDefault();
-
 
     //grab data from the input and store them in symantic variables
     var trainName = $(".trainName").val().trim();
     var trainDest = $(".destination").val().trim();
     var firstTrain = $(".firstTrain").val().trim();
     var trainFreq = $(".frequency").val().trim();
-
 
     //creat new object for a new train
     var newTrain = {
@@ -37,10 +32,8 @@ $(".submitBtn").on("click", function(event){
         freq: trainFreq 
     };
 
-
     //push newTrain object to the firebase database
     database.ref().push(newTrain);
-
 
     //clear input boxes after clicking
     $(".trainName").val("");
@@ -49,7 +42,6 @@ $(".submitBtn").on("click", function(event){
     $(".frequency").val("");
 });
 
-
 database.ref().on("child_added", function(snapshot, prevChildKey) {
     // console.log(snapshot.val());
     var trainName = snapshot.val().name;
@@ -57,47 +49,48 @@ database.ref().on("child_added", function(snapshot, prevChildKey) {
     var firstTrain = snapshot.val().time;
     var trainFreq = snapshot.val().freq;
 
-
-
-
     //Grab the current time
     var currentTime = moment().format("HH:mm a");
-    // console.log(currentTime);
-
+    console.log(currentTime);
 
     //Grab the first time the train arrives based on user input and subtract a year from it
     var firstTime = moment(firstTrain, "hh:mm").subtract(1, "years");
-    // console.log("firstTime:" + firstTime);
+    console.log("firstTime:" + firstTime);
     
-
     //Grab the difference from the current time and the first train arrival
     var trainDifference = moment().diff(moment(firstTime), "minutes");
-    // console.log("trainDifference:" + trainDifference);
+    console.log("trainDifference:" + trainDifference);
     
-
     //Use modulo to grab the remainder of the trainDifference and trainFreq
     var timeLeft = trainDifference % trainFreq;
-    // console.log("timeLeft:" + timeLeft);
+    console.log("timeLeft:" + timeLeft);
     
-
     //Grab the minutes away but subtracting the train frequency from the time left
     var minutesAway = trainFreq - timeLeft;
-    // console.log("minutesAway:" + minutesAway);
+    console.log("minutesAway:" + minutesAway);
     
-
     //Grab the next train coming
     var nextTrain = moment().add(minutesAway, "minutes").format("hh:mm a");
-    // console.log("nextTrain:" + nextTrain);
+    console.log("nextTrain:" + nextTrain);
     
 
 
+    // Grab the html table and append a table row with table data of user input and calculations
+    // from moment.js
+    $(".train-table").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" +
+    trainFreq + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td><td>");
+});
 
-// Grab the html table and append a table row with table data of user input and calculations
-// from moment.js
-$(".train-table").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" +
-trainFreq + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td><td>");
+$(".clearBtn").on("click", function() {
 
-
+    $(".trainName").val("");
+    $(".destination").val("");
+    $(".firstTrain").val("");
+    $(".frequency").val("");
 
 });
+
+
+
+
 });
